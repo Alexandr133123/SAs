8922d3a49ff3892cf1b7b7a40c5d2809e97f7d95
Alexandr133123 <63190355+Alexandr133123@users.noreply.github.com>
2020-05-11 14:44:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
 let keys = document.querySelectorAll(".key"),
     note = document.querySelector(".nowplaying"),
     hints = document.querySelectorAll(".hints"),
+    game= false,
+    arrkeys={key:[65,87,83,69,68,70,84,71,89,72,85,74,75,79,76,80,186]},
     arrnotes = { note:["A", "W", "S","E","D","F","T","G","Y","H","U","J","K","O","L","P",";"]};
-
+    rand=0;
+      life=2;
 function playNote(e) {
   let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
     key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
+
   
 
-  let keyNote = key.getAttribute("data-note");
-
   key.classList.add("playing");
+  if(game==false){
+    let keyNote = key.getAttribute("data-note");
   note.innerHTML = keyNote;
   audio.currentTime = 0;
   audio.play();
+setTimeout(pustota,6000);
+  }else{
+keyNote = key.getAttribute("data-note");
+  audio.currentTime = 0;
+  audio.play();
+  if(keyNote==arrnotes.note[rand]){
+    
+    note.innerHTML = "Harow";
+    
+
+   setTimeout(greenB(key), 1000);
+    setTimeout(randomSymbol, 1000);
+     
+    }else{
+       
+      if(life==0){
+         note.innerHTML = "Game Over";
+         life=2;
+        game=false;
+        setTimeout(pustota,3000);
+      }else{
+          key2 = document.querySelector(`.key[data-key="${arrkeys.key[rand]}"]`);
+        note.innerHTML = "Proigral";
+      life=life-1;
+      setTimeout(RedB(key), 1000);
+      setTimeout(greenB(key2), 1000);
+      setTimeout(randomSymbol, 1000);
+      }
+    }
+
+
+  }
+
 }
 
 function removeTransition(e) {
@@ -33,8 +70,42 @@ keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playNote);
 
 
-function game() {
-  let rand = Math.floor(Math.random() * arrnotes.note.length);
+function gametrue() {
+  game=true;
+ randomSymbol();
 
 }
+function randomSymbol(){
+   rand = Math.floor(Math.random() * arrnotes.note.length);
+      note.innerHTML = arrnotes.note[rand];
+}
 
+function pustota(){
+  note.innerHTML = "";
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo(a) {
+  console.log('Taking a break...');
+  await sleep(1000);
+  console.log('One second later');
+  a.style.borderColor="black";
+}
+
+
+function greenB(a){
+	
+a.style.borderColor="green";
+demo(a);
+}
+
+
+
+
+function RedB(a){
+a.style.borderColor="red";
+demo(a);
+}
